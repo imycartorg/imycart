386f9aa0531ed87943d7ad7998776aa2d3aa1405
shawn <zjuoliver@163.com>
2016-05-31 00:01:02 +0800

    去掉了s

--- shopcart/openplatform.py
+++ shopcart/openplatform.py
@@ -38,4 +38,56 @@ def weixin_login(request):
 			
 @csrf_exempt
 def weixin_call_back(request):
+	#用code换取access_tokon
+	#https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code
+	""" httplib发送https的post请求
+	headers = {"Content-type": "application/x-www-form-urlencoded; charset=UTF-8", 
+	"Accept": "*/*"} 
+	params = {'username':'xxxx'}         
+	data = urllib.urlencode(params)         
+	host = '127.0.0.1' 
+	url = '/login' 
+	conn = httplib.HTTPSConnection(host) 
+	conn.request('POST', url, data, headers)
+	
+	
+	import httplib
+	httpClient = None	
+	try:
+		httpClient = httplib.HTTPConnection('localhost', 80, timeout=30)
+		httpClient.request('GET', '/test.php')
+	
+		#response是HTTPResponse对象
+		response = httpClient.getresponse()
+		print response.status
+		print response.reason
+		print response.read()
+	except Exception, e:
+		print e
+	finally:
+		if httpClient:
+			httpClient.close()
+	"""
+	
+	app_id = 'wx3a86e89b09a7c875'
+	code = request.GET.get('code','')
+	logger.debug('获得的code:%s' % (code))
+	import http.client
+	httpClient = None	
+	try:
+		httpClient = http.client.HTTPConnection('api.weixin.qq.com', 80, timeout=30)
+		url = '/sns/oauth2/access_token?appid=%(app_id)s&secret=%(secret)s&code=%(code)s&grant_type=authorization_code' % {'app_is':app_id,'secret':secret,'code':code}
+		logger.debug('url:%s' % (url))
+		httpClient.request('GET', url)
+	
+		#response是HTTPResponse对象
+		response = httpClient.getresponse()
+		logger.debug('response.status:%s' %(response.status))
+		logger.debug('response.reason:%s' %(response.reason))
+		logger.debug('response.read():%s' %(response.read()))
+	except Exception as e:
+		logger.error(e)
+	finally:
+		if httpClient:
+			httpClient.close()	
 	return HttpResponse('OK')
