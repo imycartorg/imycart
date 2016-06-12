23ee2c5207edb82c1ca738e20fb6ea23dce7754d
shawn <zjuoliver@163.com>
2016-06-12 18:33:35 +0800

    去掉了微信

--- shopcart/index.py
+++ shopcart/index.py
@@ -33,17 +33,6 @@ def view_index(request):
 	ctx['i18n_text_example'] = _('Today is %(month)s %(day)s.') % {'month': 9, 'day': 6}
 	ctx['value'] = 2980.0
 	ctx['show_const'] = Order.ORDER_STATUS_PAYED_UNCONFIRMED
-	logger.info('i18n_text:' + ctx['i18n_text'])
-	
-	
-	#测试下微信的分享功能
-
-	
-	#要注意最后的那个 /  ！！！！！妈的，搞了两天，一直不行，就因为这个/
-	jssdk_sign = WechatJSSDKSign('http://aws.imycart.com/')
-	jssdk_config = jssdk_sign.sign()
-	ctx['wechat_config'] = jssdk_config
-	
 	return render(request,System_Config.get_template_name() + '/index.html',ctx)
 	
 	
